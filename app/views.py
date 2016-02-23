043d2021c7e1a597a3ec54354ac69e3c7eb9cba4
JayTwitty <jamariot@yahoo.com>
2016-02-23 16:33:55 -0500

    django urls, views, and templates

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+
 
 # Create your views here.
+from app.models import Stat
+
+
+def make_index(request):
+    all_players = Stat.objects.all()
+    return render_to_response('index.html',{'players': all_players} )
